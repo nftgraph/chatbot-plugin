d64812c83f5c8bdaa1502d9b9d98c116fd77059b
Shuhei Hayashi <tocoro@gmail.com>
2023-05-10 15:24:01 +0900

    不要な文字の削除

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -161,9 +161,11 @@ export const Chat: FC<Props> = memo(
             </div>
             <div className="text-center text-lg text-black dark:text-white">
               <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+              {/* 
               <div className="mb-2 font-bold">
                 Important: Chatbot UI is 100% unaffiliated with OpenAI.
               </div>
+              */}
             </div>
             <div className="text-center text-gray-500 dark:text-gray-400">
               <div className="mb-2">
